cdc0ab7007d0316a9c1d83f99168f8110f457203
Mette Thorhauge <1056403@ucn.dk>
2021-11-09 13:23:34 +0100

    Oprettelse af ModelClasses
    Custom Model Classes er oprettet og properties og constructors er opstartet.

---
+++ Service/Model/Guest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model {
+    public class Guest {
+        //Constructor for building retrieved Guests from the database
+        public Guest(int iD, string email) {
+            ID = iD;
+            Email = email;
+        }
+
+        //Cosntructor for creating and sending Guests to the database
+        public Guest(string email) {
+            Email = email;
+        }
+
+        public int ID { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Service/Model/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model {
+    public class Item {
+        //Constructor for building retrieved Items from the database
+        public Item(int iD, Wardrobe wardrobe, ItemType itemType) {
+            ID = iD;
+            Wardrobe = wardrobe;
+            ItemType = itemType;
+        }
+
+        //Constructor for creating Items to send to database
+        public Item(Wardrobe wardrobe, ItemType itemType) {
+            Wardrobe = wardrobe;
+            ItemType = itemType;
+        }
+
+        public int ID { get; set; }
+        public Wardrobe Wardrobe { get; set; }
+        public ItemType ItemType { get; set; }
+
+    }
+}
