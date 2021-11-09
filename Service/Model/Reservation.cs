913db5bf8fa4ccb23b4c18b5d7e73ab50331b129
Mette Thorhauge <1056403@ucn.dk>
2021-11-09 15:40:45 +0100

    Færdiggørelse af Model Classes
    Der er nu tilføjet properties og constructors i alle model classes.

--- Service/Model/Reservation.cs
+++ Service/Model/Reservation.cs
@@ -6,5 +6,30 @@ using System.Threading.Tasks;
 
 namespace Model {
     public class Reservation {
+        //Constructor for retrieving and building Reservations from the database
+        public Reservation(int iD, DateTime orderTime, DateTime arrivalTime, int amountOfJackets, int amountOfBags, decimal price) {
+            ID = iD;
+            OrderTime = orderTime;
+            ArrivalTime = arrivalTime;
+            AmountOfJackets = amountOfJackets;
+            AmountOfBags = amountOfBags;
+            Price = price;
+        }
+
+        //Constructor for creating and sending Reservations to the database
+        public Reservation(DateTime orderTime, DateTime arrivalTime, int amountOfJackets, int amountOfBags, decimal price) {
+            OrderTime = orderTime;
+            ArrivalTime = arrivalTime;
+            AmountOfJackets = amountOfJackets;
+            AmountOfBags = amountOfBags;
+            Price = price;
+        }
+        public int ID { get; set; }
+        public DateTime OrderTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public int AmountOfJackets { get; set; }
+        public int AmountOfBags { get; set; }
+        public decimal Price { get; set; }
+
     }
 }
