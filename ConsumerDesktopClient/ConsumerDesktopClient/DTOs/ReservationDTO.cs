87b3006ff0a4dbf92fb53521ec241d73a6ffd141
Mette Thorhauge <1056403@ucn.dk>
2021-11-24 12:25:53 +0100

    Tilføjelse af servicelag i DesktopClient
    Der er nu oprettet mappe struktur i desktop clienten, og derudover har vi skrevet en extensions class til restsharp, samt lavet en service class der connecter til APIen

--- ConsumerDesktopClient/ConsumerDesktopClient/DTOs/ReservationDTO.cs
+++ ConsumerDesktopClient/ConsumerDesktopClient/DTOs/ReservationDTO.cs
@@ -7,17 +7,23 @@ using System.Threading.Tasks;
 
 namespace ConsumerDesktopClient.DTOs
 {
-    internal class ReservationDTO
+    public class ReservationDTO
     {
         public int ReservationID { get; set; }
-        [Required]
+
+        
         public int GuestID_FK { get; set; }
 
         public DateTime OrderTime { get; set; }
-        [Required]
+
+        
         public DateTime ArrivalTime { get; set; }
-        [Required]
+
+        
         public int AmountOfJackets { get; set; }
+
+        public int AmountOfBags { get; set; } 
+
         public decimal Price { get; set; }
     }
 }
---
+++ ConsumerWebClient/APIClient/Service/IJacketOffApiClient.cs
@@ -0,0 +1,17 @@
+﻿using APIClient.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIClient {
+    public interface IJacketOffApiClient {
+        Task<IEnumerable<ReservationDTO>> GetAllReservations();
+        Task<int> CreateReservation(ReservationDTO entity);
+        Task<bool> UpdateReservation(ReservationDTO entity);
+        Task<bool> DeleteReservation(int id);
+        Task<IEnumerable<ReservationDTO>> GetReservationsByGuestEmail(string email);
+
+    }
+}
