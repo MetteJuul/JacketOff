f243c860f96f23af484d50c65c53457cbd3f2060
Mathias Møller Sørensen <Mathias_mms@yahoo.com>
2021-11-30 14:30:57 +0100

    pøls

---
+++ Service/DataAccess/Interfaces/IWardrobeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Interfaces
+{
+    internal interface IWardrobeRepository
+    {
+        Task<IEnumerable<Wardrobe>> GetAllWardrobes();
+        Task<Wardrobe> GetWardrobeById(int ID);
+        Task<bool> UpdateWardrobe(Wardrobe wardrobe);
+    }
+}
