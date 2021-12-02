92955f829982683b922cca6d94d678659da72fe1
Line Thamawat <1086259@ucn.dk>
2021-12-02 22:25:15 +0100

    Oprettet WardrobeControl.cs
    Indsat korrekte attributter

---
+++ Service/DataAccess/Model/WardrobeControl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Model {
+    public class WardrobeControl {
+        public int WardrobeID_FK { get; set; }
+        public DateTime Date { get; set; }
+        public int Count { get; set; }
+        public Int64 RowID { get; set; }
+    }
+}
