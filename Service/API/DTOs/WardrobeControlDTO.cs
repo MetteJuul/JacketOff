5a05a75ebade688f00dd9d671ccbb227737cf37d
Line Thamawat <1086259@ucn.dk>
2021-12-06 23:07:06 +0100

    Gemt rowID
    kommenteret rowID ud i DTO for at få metode til at virke

--- Service/API/DTOs/WardrobeControlDTO.cs
+++ Service/API/DTOs/WardrobeControlDTO.cs
@@ -6,7 +6,8 @@ namespace API.DTOs {
         public string WardrobeID_FK { get; set; }
         public DateTime Date { get; set; }
         public int Count { get; set; }
-        public byte[] RowID { get; set; }
+
+        //public byte[] RowID { get; set; }
         public Int64 BigRowID { get; set; }
     }
 }
