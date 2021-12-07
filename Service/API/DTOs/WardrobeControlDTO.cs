ca8b1d5b5f335bca7299eb376f4dcebb7b80c3cf
Line Thamawat <1086259@ucn.dk>
2021-12-07 08:56:44 +0100

    SLET ROWID MUHAHAHAH

--- Service/API/DTOs/WardrobeControlDTO.cs
+++ Service/API/DTOs/WardrobeControlDTO.cs
@@ -6,8 +6,5 @@ namespace API.DTOs {
         public string WardrobeID_FK { get; set; }
         public DateTime Date { get; set; }
         public int Count { get; set; }
-
-        //public byte[] RowID { get; set; }
-        public Int64 BigRowID { get; set; }
     }
 }
--- Service/DataAccess/Model/WardrobeControl.cs
+++ Service/DataAccess/Model/WardrobeControl.cs
@@ -9,7 +9,5 @@ namespace DataAccess.Model {
         public string WardrobeID_FK { get; set; }
         public DateTime Date { get; set; }
         public int Count { get; set; }
-        public byte[] RowID { get; set; }
-        public Int64 BigRowID { get; set; }
     }
 }
