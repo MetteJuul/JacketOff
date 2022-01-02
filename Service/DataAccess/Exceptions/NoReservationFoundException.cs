f187ea498f41a7dd7767ae572173234483da7000
Line Thamawat <1086259@ucn.dk>
2022-01-02 22:04:52 +0100

    NoReservationFoundException
    Egen exception til eksempel

---
+++ Service/DataAccess/Exceptions/NoReservationFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Exceptions {
+    public class NoReservationFoundException : Exception {
+
+        public NoReservationFoundException(string message, string identifier) : base(message){
+        Identifier = identifier;
+    }
+
+        // prop for at vise eksempel
+        public string Identifier { get; set; }
+    }
+}
